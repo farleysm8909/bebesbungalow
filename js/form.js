ca0b2935b09fa9be607392621ffb6cc78eb9b29d
Sam Farley <farleysm8909@uwec.edu>
2020-04-11 16:59:58 -0500

    form animation revisited

--- js/form.js
+++ js/form.js
@@ -1,4 +1,4 @@
-const form = document.getElementById('form');
+const formElement = document.getElementById('form');
 const fnameInput = document.getElementById('first-name-input');
 const lnameInput = document.getElementById('last-name-input');
 const emailInput = document.getElementById('email-input');
@@ -7,26 +7,56 @@ const messageInput = document.getElementById('message-input');
 const submitButton = document.getElementById('submit-button');
 
 const pageHeading = document.getElementById('page-heading');
+const animatedElement = document.querySelector('#form');
 
+const middleFooter = document.getElementById('mfooter');
+const leftFooter = document.getElementById('lfooter');
+const rightFooter = document.getElementById('rfooter');
+
+/* Remove form and adjust positioning of footers after form submission */
+animatedElement.addEventListener('animationend', () => {
+    formElement.style.display = "none";
+    /*leftFooter.style.position = "fixed";
+    middleFooter.style.position = "fixed";
+    rightFooter.style.position = "fixed";*/
+    leftFooter.classList.add("moveFooters");
+    middleFooter.classList.add("moveFooters");
+    rightFooter.classList.add("moveFooters");
+  });
 
 /* Form validation code adapted from Stack Overflow: 
 https://stackoverflow.com/questions/12457710/validation-of-input-text-field-in-html-using-javascript */
 
-function validateForm(form) {
+function validateForm() {
     if (isString(fnameInput)) {
         if (isString(lnameInput)) {
             if (isEmail(emailInput)) {
                 if (isPhoneNum(phoneInput)) {
                     if (isString(messageInput)) {
-                        alert("Success! The form has been submitted");
-                        return true;
+                        formElement.classList.add("fading-out");
+                        pageHeading.classList.add("fading-in");
+                        pageHeading.innerHTML = "Success! The form has been submitted";
                     }
                 }
             }
         }
     }
-    return false;
 }
+
+/* This doesn't work
+function startAnimation(element) {
+    var op = 1;  // initial opacity
+    var timer = setInterval(function () {            
+        if (op <= 0.1){
+           clearInterval(timer);
+           element.style.display = 'none';
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.1;
+    }, 50);
+    alert("reached animate");
+}*/
 
 /* ---- Form Input Validation Tests ---- */
 
